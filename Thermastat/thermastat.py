a716dcef5efb1b2b635065879de75a219fe2a31b
Jonathan L Clark <jonathan84clark@yahoo.com>
2018-11-08 06:51:12 -0800

    Fleshed out functions for all needed thermastat capabilities

--- Thermastat/thermastat.py
+++ Thermastat/thermastat.py
@@ -32,6 +32,12 @@ class S(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
+    
+    def statusToString(self, status):
+        if (status == True):
+            return "On"
+        else:
+            return "Off"
 
     def do_GET(self):
         if (self.path == "/"):
@@ -45,7 +51,11 @@ class S(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', "text/json")
             self.end_headers()
-            self.wfile.write('{"target":78, "temperature":84, "humidity":32, "weatherTemp":32, "weatherHumid":43, "windSpd":15, "acStatus":"On", "fanStatus":"Off", "heaterStatus":"Off"}')
+            jsonString = '{"target":78, "temperature":84, "humidity":32, '
+            jsonString += '"weatherTemp":32, "weatherHumid":43, "windSpd":15, '
+            jsonString += '"acStatus":"' + self.statusToString(ac_on) + '", "fanStatus":"' + self.statusToString(fan_on) + '", '
+            jsonString += '"heaterStatus":"' + self.statusToString(heat_on) + '", "autoStatus":"' + self.statusToString(auto_mode) + '"}'
+            self.wfile.write(jsonString)
         elif (os.path.isfile(file_path) and file_extension in supported_files):
             f = open(file_path, 'rb')
             self.send_response(200)
@@ -58,7 +68,57 @@ class S(BaseHTTPRequestHandler):
 
     def do_HEAD(self):
         self._set_headers()
-        
+    
+    def clear_system(self):
+        # TODO: Clear all relay states
+        pass
+    # Turns the AC on or off
+    def toggle_ac(self):
+        self.clear_system()
+        if (ac_on == True):
+            ac_on = False
+            print("AC Off")
+        else:
+            ac_on = True
+            print("AC On")
+    
+    # Turns the fan on or off
+    def toggle_fan(self):
+        self.clear_system()
+        if (fan_on == True):
+            fan_on = False
+            print("Fan Off")
+        else:
+            fan_on = True
+            print("Fan On")
+
+    # Turns the heat on or off
+    def toggle_heat(self):
+        self.clear_system()
+        if (heat_on == True):
+            heat_on = False
+            print("Heat Off")
+        else:
+            heat_on = True
+            print("Heat On")
+
+    # Turns the heat on or off
+    def toggle_auto(self):
+        self.clear_system()
+        if (auto_mode == True):
+            auto_mode = False
+            print("Auto Off")
+        else:
+            auto_mode = True
+            print("Auto On")
+
+    # Turns the heat on or off
+    def toggle_off(self):
+        self.clear_system()
+        auto_mode = False
+        # TODO: Turns everything off
+        pass
+
     def do_POST(self):
         # Doesn't do anything with posted data
         output = "Submitted"
@@ -67,9 +127,23 @@ class S(BaseHTTPRequestHandler):
         post_list = post_data.split("&") # Extract the individual post commands
         for post_str in post_list:
             key_value = post_str.split("=")
-            print(post_str)
-            # TODO: split up the post strings of the format what=param&what2=param2
-            # then send the proper commands to the thermostat thread
+            if (len(key_value) == 2):
+                if (key_value[0] == "off"):
+                    self.toggle_off()
+                elif (key_value[0] == "ac"):
+                    self.toggle_ac()
+                elif (key_value[0] == "fan"):
+                    self.toggle_fan()
+                elif (key_value[0] == "heat"):
+                    self.toggle_heat()
+                elif (key_value[0] == "auto"):
+                    self.toggle_auto()
+                elif (key_value[0] == "target"):
+                    print("Target temp set to: " + key_value[1])
+                else:
+                    print("ERROR: Invalid command: " + post_str)
+            else:
+                print("ERROR: Invalid command format: " + post_str)
             
         self._set_headers()
         self.wfile.write("<html><body><h1>" + output + "</h1></body></html>")
@@ -77,6 +151,11 @@ class S(BaseHTTPRequestHandler):
 def runServer(server_class=HTTPServer, handler_class=S, port=80):
     global target_temp
     global system_enabled
+    global ac_on
+    global heat_on
+    global fan_on
+    global variance
+    global auto_mode
 
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
