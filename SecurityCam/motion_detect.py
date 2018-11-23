ff64e4dc907175d1e22cb48059639758e1974113
Jonathan L Clark <jonathan84clark@yahoo.com>
2018-11-22 16:04:31 -0800

    Added code to allow for basic motion detector functionality

--- SecurityCam/motion_detect.py
+++ SecurityCam/motion_detect.py
@@ -1,35 +1,34 @@
-# import the necessary packages
+#################################################################
+# Motion Detector
+# DESC: Application operates a standard USB video device and runs it as
+# a motion activated security camera. 
+# Original code as pulled from online and modified
+# Date: 11/22/2018: Added code to capture frames every second when
+# motion is sensed.
+#################################################################
 from imutils.video import VideoStream
 import argparse
 import datetime
 import imutils
 import time
 import cv2
- 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video", help="path to the video file")
-ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
-args = vars(ap.parse_args())
- 
-# if the video argument is None, then we are reading from webcam
-if args.get("video", None) is None:
-	vs = VideoStream(src=0).start()
-	time.sleep(2.0)
- 
-# otherwise, we are reading from a video file
-else:
-	vs = cv2.VideoCapture(args["video"])
+
+imageDirectory = "C:\\Users\\jonat\\Desktop\\"
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
 
 nextRecordTime = 0.0
+occupiedTimeout = 0.0
+imageCount = 0
 # initialize the first frame in the video stream
 firstFrame = None
+occupied = False
 # loop over the frames of the video
 while True:
 	# grab the current frame and initialize the occupied/unoccupied
 	# text
 	frame = vs.read()
-	frame = frame if args.get("video", None) is None else frame[1]
+	frame = frame
 	text = "Unoccupied"
  
 	# if the frame could not be grabbed, then we have reached the end
@@ -62,15 +61,16 @@ while True:
 	# loop over the contours
 	for c in cnts:
 		# if the contour is too small, ignore it
-		if cv2.contourArea(c) < args["min_area"]:
-			continue
+		#if cv2.contourArea(c) < args["min_area"]:
+		#	continue
  
 		# compute the bounding box for the contour, draw it on the frame,
 		# and update the text
 		(x, y, w, h) = cv2.boundingRect(c)
 		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 		text = "Occupied"
-        # TODO: Record a video frame here
+		occupied = True
+		timeNow = time.time()
 		
 			# draw the text and timestamp on the frame
 	cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
@@ -83,10 +83,22 @@ while True:
 	cv2.imshow("Thresh", thresh)
 	cv2.imshow("Frame Delta", frameDelta)
 	timeNow = time.time()
+	# Save frames of the intruder every so often and store them to the hard drive
 	if (nextRecordTime < timeNow):
-		print("Time")
+		if (occupied == True):
+			now = datetime.datetime.now()
+			nowStr = now.strftime("%m_%d_%y_%H_%M_%S")
+			imageName =  imageDirectory + "img" + str(imageCount) + ".jpg"
+			imageCount = imageCount + 1
+			cv2.imwrite(imageName, frame)
 		nextRecordTime = timeNow + 1.0
-	#cv2.imwrite(name, frame)
+
+	# Reset occupied every few seconds
+	if (occupiedTimeout < timeNow):
+		occupied = False
+		firstFrame = None
+		occupiedTimeout = timeNow + 10.0
+
 	key = cv2.waitKey(1) & 0xFF
  
 	# if the `q` key is pressed, break from the lop
@@ -94,5 +106,5 @@ while True:
 		break
  
 # cleanup the camera and close any open windows
-vs.stop() if args.get("video", None) is None else vs.release()
+vs.stop()# if args.get("video", None) is None else vs.release()
 cv2.destroyAllWindows()
